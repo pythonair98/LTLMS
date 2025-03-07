c538de822f01c46ce06051c076c61b892e6cf800
hassanpydev <hassangames1998@gmail.com>
2025-03-07 12:40:45 +0200

    fix database issue

--- LTLMS/settings.py
+++ LTLMS/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-b5af10c63e**-73bnz^c*+rk5u%-9)!@#5uy5^#^*%r09rm7r1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0","192.168.223.8"]
+ALLOWED_HOSTS = ["0.0.0.0","192.168.223.8","213.6.135.126"]
 
 
 # Application definition
