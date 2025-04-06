bfabdced009f67af0edc609fc092adeee8c808dc
hassanpydev <hassangames1998@gmail.com>
2025-04-06 12:05:07 +0300

    fix db

--- LTLMS/settings.py
+++ LTLMS/settings.py
@@ -150,3 +150,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
