37ff0dc1d526c1c17a6bff33915594d386d7e942
hassanpydev <hassangames1998@gmail.com>
2025-04-19 10:54:03 +0300

    add new features

--- LTLMS/settings.py
+++ LTLMS/settings.py
@@ -33,8 +33,11 @@ SECRET_KEY = "django-insecure-b5af10c63e**-73bnz^c*+rk5u%-9)!@#5uy5^#^*%r09rm7r1
 DEBUG = False
 CSRF_TRUSTED_ORIGINS = [
     'https://milahaarabia.com',
-    "http://127.0.0.1",
+    "https://127.0.0.1",
     "http://localhost",
+    'https://*.milahaarabia.com',
+    "https://*.127.0.0.1",
+    "http://*.localhost",
 ]
 
 ALLOWED_HOSTS = ['*']
@@ -43,6 +46,9 @@ EMAIL_BACKEND = 'zoho_zeptomail.backend.zeptomail_backend.ZohoZeptoMailEmailBack
 DEFAULT_FROM_EMAIL = 'info@milahaarabia.com'
 ZOHO_ZEPTOMAIL_API_KEY_TOKEN = 'Zoho-enczapikey wSsVR60n+R/1XfgryWapJ+s/nV5QVF70RBwvjlP043L0HvDFp8c6kkefUAD0FfNMFDZgQTYarOggzRgG1WINitQpygoADCiF9mqRe1U4J3x17qnvhDzJXmlemxOMKo4BxQlonGJiEcEk+g=='
 ZOHO_ZEPTOMAIL_HOSTED_REGION = 'zeptomail.zoho.com'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 180
+SESSION_SAVE_EVERY_REQUEST = True
 
 # Application definition
 DATE_INPUT_FORMATS = ["%Y-%m-%d", "%m/%d/%Y", "%m/%d/%y", "%d-%b-%Y"]
