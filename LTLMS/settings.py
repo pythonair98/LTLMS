b3199eddc1c58873774f693d146b50411029b48e
hassanpydev <hassangames1998@gmail.com>
2025-04-21 15:23:54 +0300

    t

--- LTLMS/settings.py
+++ LTLMS/settings.py
@@ -219,6 +219,6 @@ LOGGING = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
