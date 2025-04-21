4c91dab72298d7ddbe01a0c9260e1b7b791d9a26
hassanpydev <hassangames1998@gmail.com>
2025-04-21 13:26:52 +0300

    t

--- LTLMS/settings.py
+++ LTLMS/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b5af10c63e**-73bnz^c*+rk5u%-9)!@#5uy5^#^*%r09rm7r1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
     'https://milahaarabia.com',
     "https://127.0.0.1",
@@ -217,7 +217,7 @@ LOGGING = {
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
