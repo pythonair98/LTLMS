1fb1d52fcc9474a638fb347d68c0f440bc48fc03
Hassan Ibrahim <halsmiri@ucas.edu.ps>
2025-07-05 12:54:21 +0300

    docker intergrations

--- init_occupations_teams.py
+++ init_occupations_teams.py
@@ -15,42 +15,36 @@ occupations_data = [
         "ar_name": "مدير دائرة التراخيص",
         "en_name": "Licensing Department Director",
         "power": 1,
-        "users_count": 0,
     },
     {
         "id": 3,
         "ar_name": "نائب مدير دائرة التراخيص",
         "en_name": "Deputy Licensing Department Director",
         "power": 2,
-        "users_count": 0,
     },
     {
         "id": 4,
         "ar_name": "رئيس قسم التراخيص",
         "en_name": "Licensing Section Head",
         "power": 3,
-        "users_count": 0,
     },
     {
         "id": 5,
         "ar_name": "مسؤول إصدار التراخيص",
         "en_name": "Licensing Officer",
         "power": 4,
-        "users_count": 0,
     },
     {
         "id": 6,
         "ar_name": "مفتش تراخيص النقل",
         "en_name": "Transport Licensing Inspector",
         "power": 5,
-        "users_count": 0,
     },
     {
         "id": 7,
         "ar_name": "موظف إدخال بيانات التراخيص",
         "en_name": "Licensing Data Entry Clerk",
         "power": 6,
-        "users_count": 0,
     },
 ]
 
@@ -61,7 +55,6 @@ for occ in occupations_data:
             "ar_name": occ["ar_name"],
             "en_name": occ["en_name"],
             "power": occ["power"],
-            "users_count": occ["users_count"],
         }
     )
     if not created:
@@ -69,7 +62,6 @@ for occ in occupations_data:
         obj.ar_name = occ["ar_name"]
         obj.en_name = occ["en_name"]
         obj.power = occ["power"]
-        obj.users_count = occ["users_count"]
         obj.save()
     print(f"Occupation {obj.en_name} ({'created' if created else 'updated'})")
 
