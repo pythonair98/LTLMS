267d30b3dac60c4feef4c01ea9ba253bc9728c5e
Hassan Ibrahim <halsmiri@ucas.edu.ps>
2025-07-06 13:28:14 +0300

    docker testing

--- LTLMS/settings_docker_simple.py
+++ LTLMS/settings_docker_simple.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-b5af10c63e**-73bnz^c*+rk5u%-9)!@#5uy5^#^*%r09rm7r1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
+DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://milahaarabia.com',
